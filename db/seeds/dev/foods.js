3191ad9693e13cdabfb7a3d23fe03f0467663f0b
Meghan Stang <mstang15@gmail.com>
2019-01-29 14:39:52 -0700

    Add seed for foods

---
+++ db/seeds/dev/foods.js
@@ -0,0 +1,24 @@
+
+exports.seed = function(knex, Promise) {
+  return knex('foods').del()
+    .then(() => {
+      return Promise.all([
+        knex('foods').insert(
+          {name: "bagel", calories: 250},
+          {name: "jelly donut", calories: 300},
+          {name: "oatmeal", calories: 150},
+          {name: "ranch salad dressing", calories: 150},
+          {name: "rice", calories: 205},
+          {name: "tuna", calories: 100},
+          {name: "spaghetti", calories: 250},
+          {name: "red wine", calories: 120},
+          {name: "pizza", calories: 300},
+          {name: "pretzels", calories: 100},
+          {name: "salsa", calories: 50},
+          {name: "cake", calories: 250}
+        )
+        .then(() => console.log('Foods Seeded :)'))
+        .catch(error => console.log(`Error seeding data: ${error}`))
+      ])
+    })
+};
