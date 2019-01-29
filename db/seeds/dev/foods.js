56b6c7f9432056b6873a6efce106afd67743f7b6
Meghan Stang <mstang15@gmail.com>
2019-01-29 16:30:13 -0700

    Add seeds for food

--- db/seeds/dev/foods.js
+++ db/seeds/dev/foods.js
@@ -1,9 +1,9 @@
 
 exports.seed = function(knex, Promise) {
-  return knex('foods').del()
+  return knex.raw('TRUNCATE foods RESTART IDENTITY CASCADE')
     .then(() => {
       return Promise.all([
-        knex('foods').insert(
+        knex('foods').insert([
           {name: "bagel", calories: 250},
           {name: "jelly donut", calories: 300},
           {name: "oatmeal", calories: 150},
@@ -16,7 +16,7 @@ exports.seed = function(knex, Promise) {
           {name: "pretzels", calories: 100},
           {name: "salsa", calories: 50},
           {name: "cake", calories: 250}
-        )
+        ], 'id')
         .then(() => console.log('Foods Seeded :)'))
         .catch(error => console.log(`Error seeding data: ${error}`))
       ])
