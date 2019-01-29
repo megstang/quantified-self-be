d26c3c14ad83ff33b6610527bb0a37f6ecf97586
Meghan Stang <mstang15@gmail.com>
2019-01-29 14:16:18 -0700

    Create migrations for foods, meals, and mealfoods

--- db/migrations/20190129133749_initial.js
+++ db/migrations/20190129133749_initial.js
@@ -1,8 +1,38 @@
 
 exports.up = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.createTable('foods', function(table){
+      table.increments('id').primary();
+      table.string('name');
+      table.integer('calories').unsigned;
+
+      table.timestamps(true,true);
+
+    }),
+
+    knex.schema.createTable('meals', function(table){
+      table.increments('id').primary();
+      table.string('name');
+
+      table.timestamps(true,true);
+    }),
+
+    knex.schema.createTable('mealFoods', function(table){
+      table.increments('id').primary();
+      table.integer('meal_id').unsigned;
+      table.foreign('meal_id').references('meals.id');
+      table.integer('food_id').unsigned;
+      table.foreign('food_id').references('foods.id');
+
+      table.timestamps(true,true);
+    })
+  ])
 };
 
 exports.down = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.dropTable('mealFoods'),
+    knex.schema.dropTable('meals'),
+    knex.schema.dropTable('foods')
+  ])
 };
