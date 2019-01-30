90d4edd7c6b8bd613a4d12dfbae2472d8aae8872
Meghan Stang <mstang15@gmail.com>
2019-01-30 10:15:10 -0700

    Add happy path for api/v1/foods endpoint

---
+++ test/routes.spec.js
@@ -0,0 +1,48 @@
+const chai = require('chai'); //require chai library
+const should = chai.should(); //call should so we can use shoulda woulda matchers like capybara
+const chaiHttp = require('chai-http'); //implement requests on server that we have locally
+const server = require('../index'); // go out and reach into server file itself
+
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
+
+chai.use(chaiHttp);
+
+describe('Client Routes', () => {
+
+});
+
+describe('API Routes', () => {
+  before((done) => {
+  database.migrate.latest()
+    .then(() => done())
+    .catch(error => {
+      throw error;
+    });
+  });
+
+  beforeEach((done) => {
+    database.seed.run()
+      .then(() => done())
+      .catch(error => {
+        throw error;
+      });
+  });
+
+  it('get api/v1/foods should return id, name, calories', done => {
+    chai.request(server)
+    .get('/api/v1/foods')
+    .end((err, response) => {
+      response.should.have.status(200);
+      response.should.be.html;
+      response.body.should.be.a('array');
+      response.body[0].should.have.property('id')
+      response.body[0].should.have.property('name')
+      response.body[0].should.have.property('calories')
+      response.body[0].name.should.equal('bagel');
+      response.body[0].calories.should.equal(250);
+    });
+    done();
+  });
+});
