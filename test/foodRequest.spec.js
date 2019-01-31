b49dc43d0e769b6b91e87d00626847b263d524e6
Meghan Stang <mstang15@gmail.com>
2019-01-30 21:02:08 -0700

    Delete routes.spec.js from test file

--- test/foodRequest.spec.js
+++ test/foodRequest.spec.js
@@ -6,7 +6,6 @@ const server = require('../index'); // go out and reach into server file itself
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../knexfile')[environment];
 const database = require('knex')(configuration);
-pry = require('pryjs');
 
 chai.use(chaiHttp);
 
@@ -33,29 +32,12 @@ describe('API Routes', () => {
 
   it('get api/v1/foods should return id, name, calories', done => {
     chai.request(server)
-    .get('/api/v1/foods')
+    .get('api/v1/foods')
     .end((err, response) => {
       response.should.have.status(200);
       response.should.be.html;
       response.body.should.be.a('array');
       response.body[0].should.have.property('id')
-      response.body[0].should.have.property('name')
-      response.body[0].should.have.property('calories')
-      response.body[0].name.should.equal('bagel');
-      response.body[0].calories.should.equal(250);
-    });
-    done();
-  });
-
-  it('get api/v1/foods should return id, name, calories', done => {
-    chai.request(server)
-    .get('/api/v1/foods/:id')
-    .end((err, response) => {
-      response.should.have.status(200);
-      response.should.be.html;
-      response.body.should.be.a('array');
-      response.body[0].should.have.property('id')
-      response.body.count.should.equal(1);
       response.body[0].should.have.property('name')
       response.body[0].should.have.property('calories')
       response.body[0].name.should.equal('bagel');
@@ -68,7 +50,7 @@ describe('API Routes', () => {
 describe('API routes without seeds', () => {
   it('get api/v1/foods returns empty array if no foods seeded',done=>{
     chai.request(server)
-    .get('/api/v1/foods') //sad path because not seeded
+    .get('api/v1/foods') //sad path because not seeded
     .end((err,response) => {
       response.should.have.status(200);
       response.body.should.be.a('array');
